ed1b1c92826ed89f16fbef63b33f16d55004a135
randy <randyr_1993@yahoo.com>
2022-10-03 18:43:43 -0400

    Completed Detention

--- Detention.java
+++ Detention.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
-        //Task 1 – Scan for the nextLine(), and print it 99 times. 
+        String input = scan.nextLine();
+        
+        for (int i = 0; i < 99; i++){
+            System.out.println(i + 1 + ". " + input);
+        }
         
     }
+    
 }
+
+// I will not forget semi-colons;
+// Prints user input 99 times, numbering each line starting from 1
