c1923ff8b39018c03ba8dcc840b4dcb97ab44237
randy <randyr_1993@yahoo.com>
2022-10-03 19:02:37 -0400

    CouningTool Completed

--- CountingTool.java
+++ CountingTool.java
@@ -4,19 +4,12 @@ public class CountingTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+    System.out.print("Hi! Choose a number to count to: ");
+    int input = scan.nextInt();
 
-        /*Task 1 – Choose a number
-
-            Ask: Hi Timmy! Choose a number to count to: 
-
-            Make sure Timmy can enter the value BESIDE the question. 
-        */
-
-        /* Task 2 – Count from 0 to that number
-
-             Let's say Timmy entered 5, you would display: 0 1 2 3 4 5 
-
-        */
+    for (int i = 0; i < input; i++){
+        System.out.print(i + 1 + " ");
+    }
 
         scan.close();
     }
