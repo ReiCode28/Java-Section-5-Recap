3ef7c9c030d520205e55550642a92d39d29719ec
randy <randyr_1993@yahoo.com>
2022-10-03 20:27:46 -0400

    Added functionality to Counter Tool

--- CounterTool.java
+++ CounterTool.java
@@ -4,30 +4,20 @@ public class CounterTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("I hear you like to count by threes\n");
-        System.out.println("Jimmy: It depends.");
+        System.out.println("You: It depends.");
         System.out.println("Oh, ok...");
 
-      /* Task 1 – Get integers from the user.
+        System.out.print("1. Pick a number to count by: ");
+        int countBy = scan.nextInt();
+        System.out.print("2. Pick a number to count from: ");
+        int countFrom = scan.nextInt();
+        System.out.print("3. Pick a number to count to: ");
+        int countTo = scan.nextInt();
 
-
-          1.   print : Pick a number to count by:
-               pick up the user's answer
+        for (int i = countFrom; i <= countTo; i += countBy){
+          System.out.print(i + " ");
+        }
         
-          2.   print : Pick a number to start counting from:
-               pick up the user's answer
-
-          3.    print : Pick a number to count to:
-                pick up the user's answer 
-                
-        */
-        
-
-      /* Task 2 – Create a for loop that:
-            1. starts from the second number.
-            2. stops at the third number. 
-            3. counts in steps of the first.
-      */
-
         scan.close();
     }
 }
