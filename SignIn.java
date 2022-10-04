d219d7472f61f8450eed1d3115d99198d8dc142e
randy <randyr_1993@yahoo.com>
2022-10-03 23:21:25 -0400

    SignIn Completed

--- SignIn.java
+++ SignIn.java
@@ -4,25 +4,22 @@ public class SignIn {
     public static void main(String[] args) {
         String username = "Samantha";
         String password = "Java <3";
-        
-        /* Task 1 
-            1. Pick up a username and password from the user.
-       */
+
         Scanner scan = new Scanner(System.in);
         System.out.println("\nWelcome to Javagram! Sign in below\n");
         System.out.print("• Username: ");
-        //pick up username
+        String userEntry = scan.nextLine();
         System.out.print("• Password: ");
-        //pick up password
+        String passEntry = scan.nextLine();
 
-        /* Task 2 
-            1. Set up a loop that keeps running while the username OR password is incorrect. 
-               When they get it wrong:
-                •  println: \nIncorrect, please try again!\n
-                •  get them to enter their username and password again
-            2. After they enter the correct information, print: 
-                   \nSign in successful. Welcome!
-       */
+        while (!userEntry.equals(username) || !passEntry.equals(password)) {
+            System.out.println("\nIncorrect, please try again!\n");
+            System.out.print("• Username: ");
+            userEntry = scan.nextLine();
+            System.out.print("• Password: ");
+            passEntry = scan.nextLine();
+        }
+        System.out.println("\nSign in sucessful. Welcome!");
 
         scan.close();
 
